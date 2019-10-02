da0257a2489715b7171e44ae7c8b6c7854542cb5
Joan P. Alvarado <joansyn.s@gmail.com>
2019-10-02 17:19:41 -0500

    Update class "Variables"

--- src/cursojava/Variables.java
+++ src/cursojava/Variables.java
@@ -5,6 +5,10 @@ public class Variables {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Vars
+		byte edad;
+		edad = 35;
+		System.out.print(edad);
 	}
 
 }
