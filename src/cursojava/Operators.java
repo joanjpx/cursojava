6f565e96f6b90ea5e1316deaeaf560ce1faddf00
Joan P. Alvarado <joansyn.s@gmail.com>
2019-10-03 15:15:18 -0500

    ++Operators Class

---
+++ src/cursojava/Operators.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package cursojava;
+
+/**
+ * @author Joan Perez
+ *
+ */
+public class Operators {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 1;
+		int b;
+		b = 2;
+		
+		int c = a+b;
+		c++;
+		c-=6;
+		
+		System.out.println(c);
+
+	}
+
+}
