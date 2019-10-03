038e4a0eaf3b0f9ee0aa6fa4cdc66f43fa53139c
Joan P. Alvarado <joansyn.s@gmail.com>
2019-10-03 15:44:23 -0500

    ++Constants Class

---
+++ src/cursojava/Constants.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package cursojava;
+
+/**
+ * @author Usuario
+ *
+ */
+public class Constants {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Constant
+		final double apulgadas = 2.54;
+		double cm = 6;
+		double resultado = cm/apulgadas;
+		//c=7;
+		System.out.println("En "+cm+" cm hay "+ resultado + " pulgadas");
+	}
+
+}
