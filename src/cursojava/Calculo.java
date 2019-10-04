bec08bffdf7508576aa74019162d62c40bea0103
Joan P. Alvarado <joansyn.s@gmail.com>
2019-10-04 10:44:03 -0500

    ++Calculo Class

---
+++ src/cursojava/Calculo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package cursojava;
+
+/**
+ * @author Usuario
+ *
+ */
+public class Calculo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double raiz = Math.sqrt(16);
+		
+		System.out.println(raiz);
+	}
+
+}
---
+++ src/cursojava/VSClass.java
@@ -0,0 +1,10 @@
+package cursojava;
+
+public class VSClass {
+
+	public static void main(String[] args) {
+        
+		System.out.print("Hola desde Visual Studio");
+	}
+
+}
