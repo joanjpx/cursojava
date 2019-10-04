49d93b91a0f060e8442152758e785d26de3d37c0
Joan P. Alvarado <joansyn.s@gmail.com>
2019-10-04 12:21:27 -0500

    ++ Substr

--- src/cursojava/Strings2.java
+++ src/cursojava/Strings2.java
@@ -4,7 +4,11 @@ public class Strings2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String str = "Hello World";
+		
+		String sub = str.substring(5);
+		
+		System.out.println(sub);
 	}
 
 }
