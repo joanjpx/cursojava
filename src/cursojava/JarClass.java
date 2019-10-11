f6a4de5921044542626c1265c621f5ff6cdbb6ca
Joan P. Alvarado <joansyn.s@gmail.com>
2019-10-11 09:23:22 -0500

    ++ Importing Classes on Java (java.util)

---
+++ src/cursojava/JarClass.java
@@ -0,0 +1,9 @@
+package cursojava;
+
+public class JarClass {
+
+    public static void main(String[] args) {
+        String name = args[0];
+        System.out.println("Hola Mundo .jar "+name);
+    }
+}
---
+++ src/cursojava/test.java
@@ -0,0 +1,15 @@
+package cursojava;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name;
+		Scanner objeto;
+		Locale objeto2;
+	}
+
+}
