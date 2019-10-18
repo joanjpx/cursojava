00ce7bd0be180913447f8f5f0877e179431cc729
Joan P. Alvarado <joansyn.s@gmail.com>
2019-10-18 10:59:35 -0500

    ++ Class Array

---
+++ src/cursojava/Array1.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package cursojava;
+
+/**
+ * @author Joan Perez
+ *
+ */
+public class Array1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] matriz = new int[5];
+		matriz[0] = 5;
+		int[] matriz2 = {1,2,3,4,5};
+		
+		for(int i = 0; i < matriz2.length; i++) {
+			
+			System.out.println(matriz2[i]);
+		}	
+
+	}
+
+}
