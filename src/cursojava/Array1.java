bd243801746cbe9117ff510e3809154aec9b85c6
Joan P. Alvarado <joansyn.s@gmail.com>
2019-10-18 11:05:46 -0500

    ++ Array Class + Using FOR loop

--- src/cursojava/Array1.java
+++ src/cursojava/Array1.java
@@ -14,13 +14,19 @@ public class Array1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/*
 		int[] matriz = new int[5];
-		matriz[0] = 5;
-		int[] matriz2 = {1,2,3,4,5};
+		matriz[0] = 77;
+		matriz[1] = 2;
+		matriz[2] = 84;
+		matriz[3] = 1;
+		matriz[4] = -9;
+		*/
+		int[] matriz = {1,2,3,4,5,23,4,6,6,7,8,43,1,12,3,5};
 		
-		for(int i = 0; i < matriz2.length; i++) {
+		for(int i = 0; i < matriz.length; i++) {
 			
-			System.out.println(matriz2[i]);
+			System.out.println(matriz[i]);
 		}	
 
 	}
---
+++ src/cursojava/Array2.java
@@ -0,0 +1,10 @@
+package cursojava;
+
+public class Array2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
