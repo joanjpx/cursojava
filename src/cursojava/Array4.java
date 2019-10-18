f5bba7fa642a690e8f6816258dea11868075942c
Joan P. Alvarado <joansyn.s@gmail.com>
2019-10-18 12:09:16 -0500

    ++ Arrays 2D

--- src/cursojava/Array4.java
+++ src/cursojava/Array4.java
@@ -4,7 +4,42 @@ public class Array4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int[][] matrix = new int[4][4];
+		
+		/*
+		matrix[0][0]=1;
+		matrix[0][1]=1;
+		matrix[0][2]=1;
+		matrix[0][3]=1;
+		
+		matrix[1][0]=1;
+		matrix[1][1]=1;
+		matrix[1][2]=1;
+		matrix[1][3]=1;
 
+		matrix[2][0]=1;
+		matrix[2][1]=2;
+		matrix[2][2]=1;
+		matrix[2][3]=1;
+
+		matrix[3][0]=1;
+		matrix[3][1]=1;
+		matrix[3][2]=1;
+		matrix[3][3]=1;
+		*/
+		
+		for(int x = 0; x < matrix.length ; x++) {
+			
+			for(int y = 0; y < matrix[x].length ; y++) {
+				
+				matrix[x][y] = (int)(Math.random()*100);
+				
+				System.out.println(matrix[x][y]);
+			}
+		}
+		
+		
 	}
 
 }
