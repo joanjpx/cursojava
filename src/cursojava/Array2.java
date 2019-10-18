5477c082cc16306e196c9a4521ea099526c7950e
Joan P. Alvarado <joansyn.s@gmail.com>
2019-10-18 11:14:49 -0500

    ++ Array 2 updated

--- src/cursojava/Array2.java
+++ src/cursojava/Array2.java
@@ -4,7 +4,22 @@ public class Array2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		/* FOREACH LOOP */
+		
+		String [] paises = {"Venezuela","Peru","Chile","Ecuador","Colombia"};
+		
+		for(int i = 0; i < paises.length; i++) {
+			
+			System.out.println(i+"--"+paises[i]);
+		}
+		
+		for(String item:paises) {
+			System.out.println(item);
+		}
+		
+		
+		
 	}
 
 }
