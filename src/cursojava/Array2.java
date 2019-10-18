0454e1611711c14e192f9e7a1e20f5b37e12e663
Joan P. Alvarado <joansyn.s@gmail.com>
2019-10-18 11:18:28 -0500

    JOptionPane + FOR

--- src/cursojava/Array2.java
+++ src/cursojava/Array2.java
@@ -1,16 +1,19 @@
 package cursojava;
 
+import javax.swing.JOptionPane;
+
 public class Array2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		/* FOREACH LOOP */
+		String [] paises = new String[5];
+		//String [] paises = {"Venezuela","Peru","Chile","Ecuador","Colombia"};
 		
-		String [] paises = {"Venezuela","Peru","Chile","Ecuador","Colombia"};
 		
 		for(int i = 0; i < paises.length; i++) {
-			
+			paises[i]=JOptionPane.showInputDialog("Introduce Pais "+(i+1));
 			System.out.println(i+"--"+paises[i]);
 		}
 		
