35c1c34def87eb7727d4b86f1baf9821aeb71bca
Joan P. Alvarado <joansyn.s@gmail.com>
2019-10-20 12:34:40 -0500

    + POO

---
+++ src/POO/Coche.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package POO;
+
+/**
+ * @author Usuario
+ *
+ */
+public class Coche {
+	
+	String marca;
+	String modelo;
+	String ruedas;
+	int ancho;
+	int alto;
+	boolean x4;
+	
+	public Coche() //same name as the class
+	{
+		marca = "TOYOTA";
+		modelo = "4RUNNER";
+		ruedas = "45/15/60";
+		ancho = 5;
+		alto = 2;
+		x4 = true;
+	}
+	
+	
+
+}
---
+++ src/POO/Uso_Coche.java
@@ -0,0 +1,15 @@
+package POO;
+import POO.Coche;
+
+public class Uso_Coche {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Coche nuevo = new Coche(); //Instance of new class "Coche"
+		
+		System.out.println(nuevo.marca);
+		
+		
+	}
+
+}
