23345e590a6277809cbfef1027d524f9b91e0d22
Joan P. Alvarado <joansyn.s@gmail.com>
2019-10-23 16:04:55 -0500

    [OOP] Working with classes

--- src/POO/Novia.java
+++ src/POO/Novia.java
@@ -3,10 +3,16 @@
  */
 package POO;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
  * @author Joan P. Alvarado
  *
  */
+//########### Class NOVIA
+
 public class Novia {
 	/**
 	 * @param args
@@ -22,19 +28,31 @@ public class Novia {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Novia lv = new Novia();
+		Auto car = new Auto("Hennessey","6661-LIM");
 		
+		
+		System.out.println("Datos del Vehiculo : "+car.getModel()+" -- "+car.getPlaca());
+		System.out.println("Datos del Propietario: "+lv.name+" "+lv.lastName);
+		System.out.println("Fecha de Renovacion: "+car.renovacion);
 	}
 
 }
 
+
+// ###### Class AUTO
 class Auto
 {
 	private String model;
 	private String placa;
-	
+	public Date renovacion;
 	public Auto(String model, String placa)
 	{
-		
+		this.model = model;
+		this.placa = placa;
+		GregorianCalendar calendario = new GregorianCalendar(2019,10-1,23,12,30,10);
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		this.renovacion = calendario.getTime();
 	}
 	
 	public String getModel()
