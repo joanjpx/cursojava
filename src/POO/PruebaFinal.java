826176c8e458d4935494ef608472b70c4bd7829b
Joan P. Alvarado <joansyn.s@gmail.com>
2019-10-24 10:42:01 -0500

    VSCode

--- src/POO/PruebaFinal.java
+++ src/POO/PruebaFinal.java
@@ -9,8 +9,14 @@ public class PruebaFinal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Empleados empleado1 = new Empleados("Joan");
-		
+		empleado1.changeSection("Sistemas");
+		System.out.println(returnEmpleadoObject(empleado1));
 		System.out.println(empleado1.getDataEmpleado());
+	}
+
+	public static Empleados returnEmpleadoObject(Empleados empleado)
+	{
+		return empleado;
 	}
 
 }
@@ -19,21 +25,24 @@ class Empleados{
 	
 	private String name;
 	private String section;
+	private int code;
 	
 	public Empleados(String nombre)
 	{
 		this.name = nombre;
-		this.section = "Administración";
+		this.section = "Administracion";
+		this.code = 6661;
 	}
 	
 	public void changeSection(String section)
 	{
 		this.section = section;
+		this.code = this.code+1;
 	}
 	
 	public String getDataEmpleado()
 	{
-		return "Nombre: "+name+" - Seccion: "+section;
+		return "Nombre: "+name+" - Seccion: "+section+" COD:"+code;
 	}
 	
 	
