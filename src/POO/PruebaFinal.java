a57049ae562cdfa12cb47e3bd65a0d906e936b2d
Joan P. Alvarado <joansyn.s@gmail.com>
2019-10-25 11:45:51 -0500

    Using STATIC and FINAL @ Class's Access Modifiers
    Using STATIC and FINAL @ Class's Access Modifiers

--- src/POO/PruebaFinal.java
+++ src/POO/PruebaFinal.java
@@ -1,5 +1,7 @@
 package POO;
 
+import javax.swing.JOptionPane;
+
 public class PruebaFinal {
 
 	public PruebaFinal() {
@@ -9,9 +11,13 @@ public class PruebaFinal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Empleados empleado1 = new Empleados("Joan",1);
+		Empleados empleado2 = new Empleados("Liz",1);
+		Empleados empleado3 = new Empleados("Irving",1);
 		empleado1.changeSection("Sistemas");
 		//System.out.println(returnEmpleadoObject(empleado1));
 		System.out.println(empleado1.getDataEmpleado());
+		System.out.println(empleado2.getDataEmpleado());
+		System.out.println(empleado3.getDataEmpleado());
 	}
 
 	public static Empleados returnEmpleadoObject(Empleados empleado)
@@ -25,20 +31,30 @@ class Empleados{
 	
 	private String name;
 	private String section;
+	//FINAL para indicar que no será modificada una vez seteada
 	private final int code;
+	//STATIC para Volver ID unica entre las diferentes instancias de clase.
+	private int id;
+	private static int idSiguiente = 1;
 	
-	public Empleados(String nombre,int id)
+	public Empleados(String nombre,int code)
 	{
 		this.name = nombre;
 		this.section = "Administracion";
-		this.code = 6661;
+		this.code = code;
+		this.id = idSiguiente;
+		this.idSiguiente++;
 	}
-	
+	/*
+	*	@param String section
+	*/
 	public void changeSection(String section)
 	{
 		this.section = section;
 	}
-
+	/*
+	*	@param int id
+	*/
 	public void setCodeEmployee(int id)
 	{
 		//this.code = id;
@@ -46,7 +62,7 @@ class Empleados{
 	
 	public String getDataEmpleado()
 	{
-		return "Nombre: "+name+" - Seccion: "+section+" COD:"+code;
+		return "Nombre: "+name+" - Seccion: "+section+" COD:"+code+" ID:"+id;
 	}
 	
 	
