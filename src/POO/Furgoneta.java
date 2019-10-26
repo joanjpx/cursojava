d9396a3eef21ea3fcd9f64cad4525e66a8c3ea71
Joan P. Alvarado <joansyn.s@gmail.com>
2019-10-26 07:23:11 -0500

    -- commit from VSCode

--- src/POO/Furgoneta.java
+++ src/POO/Furgoneta.java
@@ -1,5 +1,13 @@
 package POO;
 
 public class Furgoneta extends Coche {
-    
+    /*
+    *
+    * __Construct
+    *
+    * */
+    public Furgoneta()
+    {
+        super(); // Call to Father Class 'Coche' @ Constructor
+    }
 }
