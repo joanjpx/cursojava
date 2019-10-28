c2e7517b47a6ea27212775068bb723033d614627
Joan P. Alvarado <joansyn.s@gmail.com>
2019-10-28 10:56:24 -0500

    Multiple Constructors on Class

--- src/POO/Furgoneta.java
+++ src/POO/Furgoneta.java
@@ -1,13 +1,32 @@
 package POO;
 
 public class Furgoneta extends Coche {
+
+    private int chargeCapacity;
+    private int extraSpots;
     /*
-    *
     * __Construct
-    *
-    * */
+    */
     public Furgoneta()
     {
         super(); // Call to Father Class 'Coche' @ Constructor
+        this.chargeCapacity = 300;
+        this.extraSpots = 4;
+    }
+
+    public Furgoneta(int chargeCapacity, int extraSpots)
+    {
+        this.chargeCapacity = chargeCapacity;
+        this.extraSpots = extraSpots;
+    }
+
+    public int getChargeCapacity()
+    {
+        return this.chargeCapacity;
+    }
+
+    public int getExtraSpots()
+    {
+        return this.extraSpots;
     }
 }
--- src/POO/Uso_Coche.java
+++ src/POO/Uso_Coche.java
@@ -6,21 +6,16 @@ public class Uso_Coche {
 	public static void main(String[] args) {
 		
 		//Instance of new class "Coche"
-		
-		Coche cocheJoan = new Coche(); 
-		
+		Coche cocheJoan = new Coche();
 		Coche cocheLiz = new Coche();
-
-		Furgoneta furgon = new Furgoneta();
+		Furgoneta furgon = new Furgoneta(4,2);
 
 		cocheJoan.setMarca("FORD");
-		
 		cocheLiz.setMarca("FERRARI");
-
-		furgon.setMarca("HYUNDAI");
+		furgon.setMarca("TOYOTA");
 		//System.out.println(cocheJoan.propietario[0][1]);
 		System.out.println(furgon.getMarca());
-		
+		System.out.println(furgon.getChargeCapacity());
+		System.out.println(furgon.getExtraSpots());
 	}
-
 }
